361d14c883f375cf5fbdd526bdaf2281acf34885
cajlito <carloss.empire@gmail.com>
2021-12-23 22:25:00 -0300

    Avanzando en las practicas

--- figuras.js
+++ figuras.js
@@ -1,70 +1,94 @@
 // Código del Cuadrado
-// console.group('Cuadrados');
-// const ladoCuadrado = 5;
-// console.log(`Los lados del cuadrado miden: ${ladoCuadrado} cm`);
-
-// const perimetroCuadrado = ladoCuadrado * 4;
-// console.log(`El perímetro del cuadrado es de: ${perimetroCuadrado} cm`);
 perimetroDelCuadrado = lados => lados * 4; 
-
-// const areaCuadrado = ladoCuadrado * ladoCuadrado;
-// console.log(`El área del cuadrado es: ${areaCuadrado} cm^2`);
 areaDelCuadrado = lados => lados * lados;
-// console.groupEnd();
-
-// Código del Rectángulo
-perimetroDelRectangulo = (a, b) => (a * 2) + (b * 2);
-
-// Código del Triángulo
-console.group('Triángulos');
-const lado1Triángulo = 6;
-const lado2Triángulo = 6;
-const baseTriángulo = 4;
-const alturaTriángulo = 5.5;
-console.log(`Los lados del triángulo son: ${lado1Triángulo} cm, ${lado2Triángulo} cm, ${baseTriángulo} cm y su altura es de: ${alturaTriángulo} cm`);
-
-const perimetrotriangulo = lado1Triángulo + lado2Triángulo + baseTriángulo;
-console.log(`El perímetro del triángulo es de: ${perimetrotriangulo} cm`);
-
-const areaTriangulo = (baseTriángulo * alturaTriángulo) / 2;
-console.log(`El área del triángulo es: ${areaTriangulo} cm^2`);
-console.groupEnd();
-
-// Código del Circulo
-console.group('Círculos');
-const radioCirculo = 4;
-console.log(`El radio del círculo es de: ${radioCirculo} cm`);
-
-const diametroCirculo = radioCirculo * 2;
-console.log(`El diámetro del círculo es de: ${diametroCirculo} cm`);
-
-const PI = Math.PI;
-console.log(`El valor de PI es: ${PI} no qu`);
-
-const perimetroCirculo = diametroCirculo * PI;
-console.log(`El perímetro del círculo es de: ${perimetroCirculo} cm`);
-
-const areaCirculo = (radioCirculo * radioCirculo) * PI;
-console.log(`El área del círculo es: ${areaCirculo} cm^2`);
-console.groupEnd();
-
-// Cálculos para el Cuadrado
 function calcularPerimetroCuadrado() {
     const value = document.getElementById("sides").value;
-    const perimetroCuadrado = perimetroDelCuadrado(value);
-    alert(`El perímetro del cuadrado es de: ${perimetroCuadrado} cm`);
+    if(value <= 0) {
+        alert('No ha ingresado un valor válido')
+    } else{
+        const perimetroCuadrado = perimetroDelCuadrado(value);
+        alert(`El resultado es: ${perimetroCuadrado} cm`);
+    }
 }
-
 function calcularAreaCuadrado() {
     const value = document.getElementById("sides").value;
     const areaCuadrado = areaDelCuadrado(value);
     alert(`El área del cuadrado es: ${areaCuadrado} cm^2`);
 }
 
-// Cálculos para el Rectángulo
+// Código del Rectángulo
+perimetroDelRectangulo = (a, b) => (a * 2) + (b * 2);
+areaDelRectangulo = (a, b) => a * b;
 function calcularPerimetroRectangulo() {
-    const sideA = document.getElementById("sideA");
-    const sideB = document.getElementById("sideB");
+    const sideA = document.getElementById("sideA").value;
+    const sideB = document.getElementById("sideB").value;
     const perimetroRectangulo = perimetroDelRectangulo(sideA, sideB);
     alert(`El perímetro del rectángulo es de: ${perimetroRectangulo} cm`);
+}
+function calcularAreaRectangulo() {
+    const sideA = document.getElementById("sideA").value;
+    const sideB = document.getElementById("sideB").value;
+    const areaRectangulo = areaDelRectangulo(sideA, sideB);
+    alert(`El área del rectángulo es: ${areaRectangulo} cm^2`);
+}
+
+// Código del Circulo
+diametroDelCirculo = r => r * 2;
+perimetroDelCirculo = r => (r * 2) * Math.PI;
+areaDelCirculo = r => r * r * Math.PI;
+function calcularDiametroCirculo() {
+    const r = document.getElementById("radius").value;
+    const diametroCirculo = diametroDelCirculo(r);
+    alert(`El diámetro del círculo es: ${diametroCirculo} cm`);
+}
+function calcularPerimetroCirculo() {
+    const r = document.getElementById("radius").value;
+    const perimetroCirculo = perimetroDelCirculo(r);
+    alert(`El perímetro del círculo es: ${perimetroCirculo} cm`);
+}
+function calcularAreaCirculo() {
+    const r = document.getElementById("radius").value;
+    const areaCirculo = areaDelCirculo(r);
+    alert(`El área del círculo es: ${areaCirculo} cm^2`);
+}
+
+// Código del Triángulo
+alturaDelTrialgulo = (a, b) => Math.sqrt( Math.pow(a, 2) - ( Math.pow(b, 2) / 4) );
+perimetroDelTriangulo = (a, b) => (parseInt(a) * 2) + parseInt(b);
+areaDeltriangulo = (a, b) => ( (b * ( Math.sqrt( Math.pow(a, 2) - ( Math.pow(b, 2) / 4) ) )) / 2 );
+
+function calcularAlturaTriangulo() {
+    const a = document.getElementById("ladoA").value;
+    const b = document.getElementById("ladoB").value;
+    const c = document.getElementById("ladoC").value;
+    if( (a === b) && (a !== c) && (b !== c)) {
+        const h = alturaDelTrialgulo(a, c);
+        alert(`La altura de tu triángulo es: ${h} cm`);
+    } else {
+        alert("No es un triangulo isósceles");
+    }
+}
+
+function calcularPerimetroTriangulo() {
+    const a = document.getElementById("ladoA").value;
+    const b = document.getElementById("ladoB").value;
+    const c = document.getElementById("ladoC").value;
+    if( (a === b) && (a !== c) && (b !== c)) {
+        const p = perimetroDelTriangulo(a, c);
+        alert(`La altura de tu triángulo es: ${p} cm`);
+    } else {
+        alert("No es un triangulo isósceles");
+    }
+}
+
+function calcularAreaTriangulo() {
+    const a = document.getElementById("ladoA").value;
+    const b = document.getElementById("ladoB").value;
+    const c = document.getElementById("ladoC").value;
+    if( (a === b) && (a !== c) && (b !== c)) {
+        const area = areaDeltriangulo(a, c);
+        alert(`El área de tu triángulo es: ${area} cm^2`);
+    } else {
+        alert("No es un triangulo isósceles");  
+    }
 }
